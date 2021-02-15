6db15fe71aca955e166ed4a51e6066a0d26f6072
Edgar Quiroz <eq24f02@protonma.ch>
2021-02-15 17:13:09 -0600

    Generar código de cuádruplas.

--- Proyecto/src/codigo.cpp
+++ Proyecto/src/codigo.cpp
@@ -17,17 +17,92 @@
 #define build_ptr_copy(x, y) x + " = " + "&" + y
 #define build_val_copy(x, y) x + " = " + "*" + y
 #define build_val_assig(x, y) x + "*" + " = " + y
+#define build_label(l) l + ":"
 
 using namespace std;
 
 ostream &operator<<(ostream &out, const Cuadrupla &c) {
-  out << c.op << " with ";
-  out << c.t1;
-  if (c.t2 != "")
-    out << " and " << c.t2;
-  if (c.res != "")
-    out << " at " << c.res;
-  out << endl;
+  string s = "";
+  string op = "";
+  switch (c.op) {
+  case C_COPY:
+    s = build_copy(c.t1, c.res);
+    break;
+  case C_PLUS:
+    op = "+";
+    s = build_assig(op, c.res, c.t1, c.t2);
+    break;
+  case C_MINUS:
+    op = "-";
+    s = build_assig(op, c.res, c.t1, c.t2);
+    break;
+  case C_MUL:
+    op = "*";
+    s = build_assig(op, c.res, c.t1, c.t2);
+    break;
+  case C_DIV:
+    op = "/";
+    s = build_assig(op, c.res, c.t1, c.t2);
+    break;
+  case C_GOTO:
+    s = build_goto(c.res);
+    break;
+  case C_IF:
+    s = build_if(c.t1, c.res);
+    break;
+  case C_IF_FALSE:
+    s = build_if_false(c.t1, c.res);
+    break;
+  case C_IF_GE:
+    op = ">";
+    s = build_if_rel(op, c.t1, c.t2, c.res);
+    break;
+  case C_IF_GEQ:
+    op = ">=";
+    s = build_if_rel(op, c.t1, c.t2, c.res);
+    break;
+  case C_IF_LE:
+    op = "<";
+    s = build_if_rel(op, c.t1, c.t2, c.res);
+    break;
+  case C_IF_LEQ:
+    op = "<=";
+    s = build_if_rel(op, c.t1, c.t2, c.res);
+    break;
+  case C_IF_EQ:
+    op = "==";
+    s = build_if_rel(op, c.t1, c.t2, c.res);
+    break;
+  case C_IF_NEQ:
+    op = "!=";
+    s = build_if_rel(op, c.t1, c.t2, c.res);
+    break;
+  case C_IDX_COPY:
+    s = build_idx_copy(c.res, c.t1, c.t2);
+    break;
+  case C_IDX_ASSIG:
+    s = build_idx_assig(c.res, c.t1, c.t2);
+    break;
+  case C_PARAM:
+    s = build_param(c.t1);
+    break;
+  case C_CALL:
+    s = build_call(c.t1, c.t2);
+    break;
+  case C_PTR_COPY:
+    s = build_ptr_copy(c.res, c.t1);
+    break;
+  case C_VAL_COPY:
+    s = build_val_copy(c.res, c.t1);
+    break;
+  case C_VAL_ASSIG:
+    s = build_val_assig(c.res, c.t1);
+    break;
+  case C_LABEL:
+    s = build_label(c.t1);
+    break;
+  }
+  out << s << endl;
   return out;
 }
 
